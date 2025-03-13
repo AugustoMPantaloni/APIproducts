ebc193f69c62715422def4fe8c6809e7ed00163d
Augusto Pantaloni <AugustoMPantaloni@gmail.com>
2025-03-13 19:31:40 -0300

    APIproducts

--- src/app.js
+++ src/app.js
@@ -1,17 +1,38 @@
 const express = require ("express");
 const app = express();
-const routerProducts = require ("./Routers/products")
-const routerCarts = require ("./Routers/carts")
+const path = require("path");
+const routerProducts = require ("./Routers/products");
+const routerCarts = require ("./Routers/carts");
+const exphbs = require ("express-handlebars");
+const controllerProducts = require ("./controllers/productsController")//Se creo un controlador para mostrar los productos.
 
+//Middleware para parseo de JSON
 app.use(express.json());
 
+//Configuracion para servir archivos estaticos
+app.use(express.static(path.join(__dirname, "..", "public"))); 
+
+//Configuracion Handlebars
+app.engine("handlebars", exphbs.engine());
+app.set("view engine", "handlebars");
+
+//Definimos la carpeta de rutas
+app.set("views", path.join(__dirname, "views"));
+
+//Routers
 app.use("/api/products", routerProducts)
 app.use("/api/carts", routerCarts);
+app.use("/realtimeproducts,  ") //Falta agregar el router con los webSockets
 
-app.get("/", (req, res)=>{
-    res.send("Servidor en linea");
+//Ruta raiz
+app.use("/", controllerProducts.getProducts)
+
+//Ruta Para los productos en tiempo real
+app.get("/realtimeproducts",(req, res)=>{
+    res.render("realTimeProducts",)
 })
 
+//Servidor
 const PORT = 8080;
 app.listen (PORT,()=>{
     console.log(`Servidor corriendo en puerto ${PORT}`);
---
+++ src/controllers/productsController.js
@@ -0,0 +1,14 @@
+const productManager = require("../Models/productManager");
+const instanciaProducts = new productManager();
+
+const getProducts = async (req, res) => {
+    try {
+        const products = await instanciaProducts.getAllProducts();
+        res.render("home", { products });
+    } catch (error) {
+        console.error("Error al obtener productos:", error);
+        res.status(500).send("Error interno del servidor");
+    }
+};
+
+module.exports = {getProducts,};
